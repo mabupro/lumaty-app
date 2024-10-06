a79c6a1ff21411cb172d644f10d41627c3f50124
mabupro <mabupro787@gmail.com>
2024-10-06 15:22:03 +0900

    fix/一度PUTをコメントアウト

--- app/api/festival/[id]/route.ts
+++ app/api/festival/[id]/route.ts
@@ -30,55 +30,55 @@ export const GET = async (req: Request, res: NextResponse) => {
 }
 
 // 祭り、編集用
-export const PUT = async (req: Request) => {
-	try {
-		const url = new URL(req.url)
-		const id = Number(url.pathname.split('/festival/')[1])
-		const {
-			name,
-			country,
-			prefecture,
-			city_town,
-			representative,
-			overview,
-			history,
-			start_date,
-			end_date,
-			locations,
-			news,
-			images,
-		} = await req.json()
+// export const PUT = async (req: Request) => {
+// 	try {
+// 		const url = new URL(req.url)
+// 		const id = Number(url.pathname.split('/festival/')[1])
+// 		const {
+// 			name,
+// 			country,
+// 			prefecture,
+// 			city_town,
+// 			representative,
+// 			overview,
+// 			history,
+// 			start_date,
+// 			end_date,
+// 			locations,
+// 			news,
+// 			images,
+// 		} = await req.json()
 
-		// Festival データを作成する
-		const festival = await prisma.festival.update({
-			data: {
-				name,
-				country,
-				prefecture,
-				city_town,
-				representative,
-				overview,
-				history,
-				start_date:start_date ? new Date(start_date) : null,
-				end_date: end_date ? new Date(end_date) : null,
-				// リレーションのデータはネストして作成
-				locations: {
-					create: locations, // 例: locationsが[{...}, {...}]の形式であること
-				},
-				news: {
-					create: news,
-				},
-				images: {
-					create: images,
-				},
-			},
-			where: { id },
-		})
-		return NextResponse.json({ message: 'Success', festival }, { status: 200 })
-	} catch (error) {
-		console.error('Error creating festival:', error)
-		return NextResponse.json({ message: 'Error', error }, { status: 500 })
-	} finally {
-		await prisma.$disconnect()
-	}
-}
+// 		// Festival データを作成する
+// 		const festival = await prisma.festival.update({
+// 			data: {
+// 				name,
+// 				country,
+// 				prefecture,
+// 				city_town,
+// 				representative,
+// 				overview,
+// 				history,
+// 				start_date: start_date ? new Date(start_date) : null,
+// 				end_date: end_date ? new Date(end_date) : null,
+// 				// リレーションのデータはネストして作成
+// 				locations: {
+// 					create: locations, // 例: locationsが[{...}, {...}]の形式であること
+// 				},
+// 				news: {
+// 					create: news,
+// 				},
+// 				images: {
+// 					create: images,
+// 				},
+// 			},
+// 			where: { id },
+// 		})
+// 		return NextResponse.json({ message: 'Success', festival }, { status: 200 })
+// 	} catch (error) {
+// 		console.error('Error creating festival:', error)
+// 		return NextResponse.json({ message: 'Error', error }, { status: 500 })
+// 	} finally {
+// 		await prisma.$disconnect()
+// 	}
+// }
