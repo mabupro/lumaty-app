1d439e04e2f7fbc2b7415e3f9466a641da5599b8
mabupro <mabupro787@gmail.com>
2024-10-06 16:52:14 +0900

    fix/route

--- app/api/festival/[id]/route.ts
+++ app/api/festival/[id]/route.ts
@@ -1,25 +1,26 @@
 import { NextResponse } from 'next/server'
 import prisma from '@/prisma/prismaClient'
 
-// プログラム取得
-export const GET = async (req: Request) => {
+// 祭りごとに取得用
+export const GET = async (req: Request, res: NextResponse) => {
 	try {
 		const url = new URL(req.url)
-		const festivalId = Number(url.pathname.split('/api/program/')[1])
+		const id = Number(url.pathname.split('/festival/')[1])
 
-		if (Number.isNaN(festivalId)) {
+		if (Number.isNaN(id)) {
+			// idが数値でない場合のチェック
 			return NextResponse.json({ message: 'Invalid festival ID' }, { status: 400 })
 		}
 
-		const programs = await prisma.program.findMany({
-			// 型を明示
-			where: { festival_id: festivalId },
-			include: { location: true },
-		})
+		const festival = await prisma.festival.findFirst({ where: { id } })
 
-		return NextResponse.json({ message: 'Success', programs }, { status: 200 })
+		if (!festival) {
+			return NextResponse.json({ message: 'festivals not found' }, { status: 404 })
+		}
+
+		return NextResponse.json({ message: 'Success', festival }, { status: 200 })
 	} catch (error) {
-		console.error('Error fetching programs:', error)
+		console.error('Error fetching festivals:', error)
 		return NextResponse.json({ message: 'Error', error }, { status: 500 })
 	} finally {
 		await prisma.$disconnect()
