5e4f97f0f793714ce4c36f0f18e35c6e127a7839
mabupro <mabupro787@gmail.com>
2024-10-10 11:11:53 +0900

    fix/prisma

--- prisma/prismaClient.ts
+++ prisma/prismaClient.ts
@@ -1,14 +1,17 @@
 import { PrismaClient } from '@prisma/client'
 
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
+
 let prisma: PrismaClient
 
 if (process.env.NODE_ENV === 'production') {
 	prisma = new PrismaClient()
 } else {
-	if (!globalThis.prisma) {
-		globalThis.prisma = new PrismaClient()
+	// 開発環境では、既に存在するインスタンスを再利用
+	if (!globalForPrisma.prisma) {
+		globalForPrisma.prisma = new PrismaClient()
 	}
-	prisma = globalThis.prisma
+	prisma = globalForPrisma.prisma
 }
 
 export default prisma
