5f2f1c09e6dcf129d6395ec55aa46696b8df051a
mabupro <mabupro787@gmail.com>
2024-10-10 10:21:45 +0900

    fix/globalにprismaのtypesを追加

--- prisma/prismaClient.ts
+++ prisma/prismaClient.ts
@@ -1,17 +1,14 @@
 import { PrismaClient } from '@prisma/client'
 
-// PrismaClient をグローバル変数として管理
 let prisma: PrismaClient
 
 if (process.env.NODE_ENV === 'production') {
-	// 本番環境では新しいインスタンスを生成
 	prisma = new PrismaClient()
 } else {
-	// 開発環境ではグローバル変数に PrismaClient を保存
-	if (!global.prisma) {
-		global.prisma = new PrismaClient()
+	if (!globalThis.prisma) {
+		globalThis.prisma = new PrismaClient()
 	}
-	prisma = global.prisma
+	prisma = globalThis.prisma
 }
 
 export default prisma
