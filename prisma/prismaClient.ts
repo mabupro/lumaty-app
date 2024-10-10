6b10c2e98852f6c39fa41c6aa825bfadbf8d8da1
mabupro <mabupro787@gmail.com>
2024-10-10 11:23:22 +0900

    fix/以前の状態に戻す

--- prisma/prismaClient.ts
+++ prisma/prismaClient.ts
@@ -1,14 +1,5 @@
 import { PrismaClient } from '@prisma/client'
 
-let prisma: PrismaClient
-
-if (process.env.NODE_ENV === 'production') {
-	prisma = new PrismaClient()
-} else {
-	if (!globalThis.prisma) {
-		globalThis.prisma = new PrismaClient()
-	}
-	prisma = globalThis.prisma
-}
+const prisma = new PrismaClient()
 
 export default prisma
